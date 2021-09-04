82c6f0b009867602d99c3d6fe2f14904e6ad3d9e
Matt Schneiderman <schneidermanmail@gmail.com>
2021-09-04 15:15:25 -0400

    Add basic package/tests

---
+++ src/sandwichchampion/domain/model.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+
+class Sandwich:
+    def __init__(self, name: str):
+        self.name = name
---
+++ tests/unit/test_model.py
@@ -0,0 +1,7 @@
+from sandwichchampion.domain.model import Sandwich
+
+class TestSandwich:
+    def test_name(self):
+        sw = Sandwich("Grilled Cheese")
+
+        assert sw.name == "Grilled Cheese"
