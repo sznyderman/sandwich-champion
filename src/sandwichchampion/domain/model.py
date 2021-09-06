e2bf3896f1fd8579083adba301e82df484cd0208
Matt Schneiderman <schneidermanmail@gmail.com>
2021-09-06 15:50:21 -0400

    Add ratings

--- src/sandwichchampion/domain/model.py
+++ src/sandwichchampion/domain/model.py
@@ -4,3 +4,16 @@ from __future__ import annotations
 class Sandwich:
     def __init__(self, name: str):
         self.name = name
+        self.rating = 1000
+
+
+def update_ratings(winner: Sandwich, loser: Sandwich):
+    def calculate_change():
+        K = 20
+        ratio = (loser.rating - winner.rating) / 400
+        expected_score = 1 / (1 + 10 ** ratio)
+        return round(K * (1 - expected_score), 1)
+
+    change = calculate_change()
+    winner.rating += change
+    loser.rating -= change
--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,4 +1,5 @@
-from sandwichchampion.domain.model import Sandwich
+import pytest
+from sandwichchampion.domain.model import Sandwich, update_ratings
 
 
 class TestSandwich:
@@ -6,3 +7,27 @@ class TestSandwich:
         sw = Sandwich("Grilled Cheese")
 
         assert sw.name == "Grilled Cheese"
+
+    def test_initial_rating_is_1000(self):
+        sw = Sandwich("Grilled Cheese")
+
+        assert sw.rating == 1000
+
+    @pytest.mark.parametrize(
+        "starting_ratings, updated_ratings",
+        (
+            ((1000, 1000), (1010, 990)),
+            ((2000, 2000), (2010, 1990)),
+            ((1500, 2000), (1518.9, 1981.1)),
+            ((2000, 1500), (2001.1, 1498.9)),
+        ),
+    )
+    def test_update_rating(self, starting_ratings, updated_ratings):
+        sw1 = Sandwich("sandwich 1")
+        sw2 = Sandwich("sandwich 2")
+
+        sw1.rating, sw2.rating = starting_ratings
+
+        update_ratings(winner=sw1, loser=sw2)
+
+        assert (sw1.rating, sw2.rating) == updated_ratings
